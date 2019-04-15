2868f8ea0bf2b46a9acb67052284175657900f18
bhordhundey <adetunjibodunde1@gmail.com>
2019-04-15 16:32:52 +0100

    Updates

---
+++ precache-manifest.4bf017ec8f012e8d22ff05cc816e31bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e8df3eef6bbfa95dbd8",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4e8df3eef6bbfa95dbd8",
+    "url": "/contactmanager/static/js/main.68f8305c.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "c531fe537068a2bcb798",
+    "url": "/contactmanager/static/css/2.07dbcd5a.chunk.css"
+  },
+  {
+    "revision": "c531fe537068a2bcb798",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "0642be913784a42bbd176247d58d5ff6",
+    "url": "/contactmanager/index.html"
+  }
+];
